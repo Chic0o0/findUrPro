74a6e278112d92038e1e87e3810875ffe0f1262a
Andres Chico <chicokds@gmail.com>
2022-05-11 11:59:10 +0200

    Added a navbar, app-navbar not working

--- resources/frontend/angular/src/app/public/public.module.ts
+++ resources/frontend/angular/src/app/public/public.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublicComponent
+  ],
   imports: [
     CommonModule,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    LogoutComponent
+    LogoutComponent ,
+    NavbarComponent
   ]
 })
 export class PublicModule { }
