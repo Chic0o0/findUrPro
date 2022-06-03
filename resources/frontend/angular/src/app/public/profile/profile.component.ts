f56225fee165fe2b30badc339c9c26d45308442e
Andres Chico <chicokds@gmail.com>
2022-06-03 14:59:17 +0200

    Upload media left and then just producing

--- resources/frontend/angular/src/app/public/profile/profile.component.ts
+++ resources/frontend/angular/src/app/public/profile/profile.component.ts
@@ -8,17 +8,26 @@ import { BackTalkService } from 'src/app/services/back-talk.service';
 })
 export class ProfileComponent implements OnInit {
   
+  profileData:any = {};
+
   constructor(private servicio: BackTalkService) {
+    
+  }
+
+  
+  ngOnInit(): void {
     // Intento de mostrar en el componente datos del usuario, salio mal
+
     // this.servicio.getUser().subscribe( params => {
     //   this.datosUser = this.servicio.getUser();
     // })
 
     // this.datosUser = this.servicio.getUser().subscribe(datosUser=>{return datosUser});
     // console.log(this.datosUser);
-  }
 
-  ngOnInit(): void {
+    this.servicio.getUser().subscribe( params => {
+      this.profileData = params;
+    })
   }
 
 }
