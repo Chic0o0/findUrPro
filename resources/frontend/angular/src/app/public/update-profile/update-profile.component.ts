81391e2192c159da2b822a92829aae8d6d687ce5
Chic0o0 <68516042+Chic0o0@users.noreply.github.com>
2022-06-07 19:33:35 +0200

    Started with DOM in updateProfile

--- resources/frontend/angular/src/app/public/update-profile/update-profile.component.ts
+++ resources/frontend/angular/src/app/public/update-profile/update-profile.component.ts
@@ -25,6 +25,44 @@ export class UpdateProfileComponent implements OnInit {
   }
 
   updateInfo(){
-    
+    let info = document.querySelectorAll("input");
+    info.forEach(element => {
+      console.log(element.value);
+    });
+
+    let userInfo = {
+      name: info[0].value,
+      surname: info[0].value,
+      email: info[0].value,
+      number: document.getElementById("i4")?.textContent,
+      CP: document.getElementById("i5")?.textContent,
+      adress: document.getElementById("i6")?.textContent,
+      city: document.getElementById("i7")?.textContent,
+      country: document.getElementById("i8")?.textContent,
+      password: document.getElementById("i9")?.textContent
+    }
+    // console.log(userInfo);
+    // this.servicio.addUser(usuario)
+    // .subscribe(datos=>{console.log(datos);});
+  }
+  insertAdd(){
+    let info = document.getElementsByClassName("updateInfo");
+    let addInfo = {
+      photo: info[0].nodeValue,
+      text: info[1].nodeValue,
+      title: info[2].nodeValue,
+      prize: info[3].nodeValue,
+    }
+    console.log(addInfo);
+  }
+  updateAdds(){
+    let info = document.getElementsByClassName("updateInfo");
+    let addInfo = {
+      photo: info[0].nodeValue,
+      text: info[1].nodeValue,
+      title: info[2].nodeValue,
+      prize: info[3].nodeValue,
+    }
+    console.log(addInfo);
   }
 }
